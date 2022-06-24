0e5f24d3753a7dd157741639fed7e367347924c1
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-06-24 10:55:09 -0300

    fix: try to fix font-family again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
