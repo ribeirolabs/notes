5dd5a80f044ebe2d75279d6890498e2566ddc270
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-06-23 23:17:14 -0300

    feat: setup DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["JetBrains Mono", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: [
+      {
+        lofi: {
+          ...require("daisyui/src/colors/themes")["[data-theme=lofi]"],
+          "base-100": "#202122",
+          "base-content": "#ccc",
+        },
+      },
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+  },
 };
