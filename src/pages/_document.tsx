50ab7e7f0c6518d125b881cca9c65652bad191d7
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-06-24 10:59:04 -0300

    fix: font loading changed in Next.js 10.2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,20 @@ export default function Document() {
     <Html data-theme="lofi">
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,200;0,400;0,600;0,800;1,200;1,400;1,600;1,800&display=swap"
-          rel="stylesheet"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
         />
+        <style data-href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,200;0,400;0,600;0,800;1,200;1,400;1,600;1,800&display=swap">
+          {`
+          @font-face {
+            font-family: 'JetBrains Mono';
+            font-style: medium;
+            font-weight: normal;
+            font-display: swap;
+          }
+          `}
+        </style>
       </Head>
       <body>
         <Main />
