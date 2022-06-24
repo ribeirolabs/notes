03ee59d014a4151d07763f2a70965ac63b063cfb
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-06-24 10:38:12 -0300

    fix: font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["JetBrains Mono", ...defaultTheme.fontFamily.sans],
-      },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
